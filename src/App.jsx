ad10a999c625dee2d6e6fa8d851534db4edbe7df
Ruthless Calm <ruthlesscalm@proton.me>
2026-05-06 22:02:56 +0530

    Add card component and combobox from shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxContent,
+  ComboboxItem,
+  ComboboxEmpty,
+  ComboboxList,
+} from '@/components/ui/combobox';
+
+const semisters = [];
 
 const App = () => {
-  return <main></main>;
+  return (
+    <main>
+      <Card>
+        <Combobox items={semisters}>
+          <ComboboxInput placeholder="Select the semister" />
+          <ComboboxContent>
+            <ComboboxEmpty>No items found.</ComboboxEmpty>
+            <ComboboxList>
+              {(item) => (
+                <ComboboxItem key={item} value={item}>
+                  {item}
+                </ComboboxItem>
+              )}
+            </ComboboxList>
+          </ComboboxContent>
+        </Combobox>
+      </Card>
+    </main>
+  );
 };
 
 export default App;
