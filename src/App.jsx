ef90b6bca03157024e2d307284ee2c15fffd3384
Ruthless Calm <ruthlesscalm@proton.me>
2026-05-07 09:31:52 +0530

    Add semester select field and cycle select field

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,69 @@
 import { Card } from '@/components/ui/card';
 import {
-  Combobox,
-  ComboboxInput,
-  ComboboxContent,
-  ComboboxItem,
-  ComboboxEmpty,
-  ComboboxList,
-} from '@/components/ui/combobox';
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+} from '@/components/ui/select';
+import { useState } from 'react';
 
-const semisters = [];
+const semesters = [
+  '1st Sem',
+  '2nd Sem',
+  '3rd Sem',
+  '4th Sem',
+  '5th Sem',
+  '6th Sem',
+  '7th Sem',
+  '8th Sem',
+];
 
 const App = () => {
+  const [selectedSemester, setSelectedSemester] = useState('');
+  const [selectedCycle, setSelectedCycle] = useState('');
   return (
     <main>
       <Card>
-        <Combobox items={semisters}>
-          <ComboboxInput placeholder="Select the semister" />
-          <ComboboxContent>
-            <ComboboxEmpty>No items found.</ComboboxEmpty>
-            <ComboboxList>
-              {(item) => (
-                <ComboboxItem key={item} value={item}>
-                  {item}
-                </ComboboxItem>
-              )}
-            </ComboboxList>
-          </ComboboxContent>
-        </Combobox>
+        <Select
+          value={selectedSemester}
+          onValueChange={(value) => setSelectedSemester(value)}
+        >
+          <SelectTrigger className="w-full max-w-48">
+            <SelectValue placeholder="Select Your semester" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel className="sr-only">Semesters</SelectLabel>
+              {semesters.map((v) => {
+                return (
+                  <SelectItem value={v.slice(0, 1)} key={v.slice(0, 1)}>
+                    {v}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        {(selectedSemester === '1' || selectedSemester === '2') && (
+          <Select
+            value={selectedCycle}
+            onValueChange={(value) => setSelectedCycle(value)}
+          >
+            <SelectTrigger className="w-full max-w-48">
+              <SelectValue placeholder="Select the cycle" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel className="sr-only">Cycles</SelectLabel>
+                <SelectItem value="physics">Physics cycle</SelectItem>
+                <SelectItem value="chemistry">Chemistry cycle</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        )}
       </Card>
     </main>
   );
